e0f718eac22c908b6b1ff33ab4e92c06e7fdc05d
Your Name <343019037@qq.com>
2018-07-05 14:24:02 +0800

    test program

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        URL url = null;
+        URLConnection urlConnection = null;
+        BufferedReader br  = null;
+        PrintWriter pw = null;
+        String regex = "https://[\\w+\\.?/?]+\\.[A-Za-z]+";
+        try {
+
+            url = new URL("https://bbs.csdn.net/topics/390765699");
+            urlConnection = url.openConnection();
+            pw = new PrintWriter(new FileWriter("D:/taobaoUrl.txt"),true);
+            br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            Pattern p = Pattern.compile(regex);
+            String buff = null;
+            while((buff = br.readLine()) != null)
+            {
+                Matcher mat_m = p.matcher(buff);
+                while(mat_m.find()){
+                    pw.println(mat_m.group());
+                }
+            }
+            System.out.print("worm sucuess");
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                br.close();
+            }catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            pw.close();
+        }
+
+    }
+}
